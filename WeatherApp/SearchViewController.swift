17c9bbe36fc7a8e3710ab0ab20737f7d5be46c13
Jayant Arora <jayant.arora@vtinfo.com>
2018-02-12 15:58:09 -0500

    add some test code for sharing state, and testing REST calls

--- WeatherApp/SearchViewController.swift
+++ WeatherApp/SearchViewController.swift
@@ -8,16 +8,95 @@
 
 import Foundation
 import UIKit
+import MapKit
 
-class SearchView: UIViewController, UISearchBarDelegate {
+class SearchView: UIViewController, UISearchBarDelegate, MKLocalSearchCompleterDelegate {
+    
+    // MARK: - Default properties
+    // TODO: - UPDATE LOC FROM CORELOCATION
+    private static var currentLoc = "TEST"
+    
+    // MARK: - Default location servies
+    class func getCurrentLoc() -> String {
+        return currentLoc
+    }
+    
+    
+    @IBAction func closeSearch(_ sender: UIBarButtonItem) {
+        performSegue(withIdentifier: "placeSelected", sender: self)
+    }
+    
+    // MARK: -  Search Bar
     
     @IBOutlet weak var search: UISearchBar!
     
     override func viewDidLoad() {
         search.delegate = self
+        searchCompleter.delegate = self
     }
     
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
         print(searchText)
+        searchCompleter.queryFragment = searchText
+    }
+    
+    // MARK: - MapKit
+    
+    var searchCompleter = MKLocalSearchCompleter()
+    var searchResults = [MKLocalSearchCompletion]()
+    
+    func completerDidUpdateResults(_ completer: MKLocalSearchCompleter) {
+        searchResults = completer.results
+        searchResultsViewer.reloadData()
+        print(searchResults)
+    }
+    
+    func completer(_ completer: MKLocalSearchCompleter, didFailWithError error: Error) {
+        print(error)
+    }
+    
+    // MARK: - Table View
+
+    @IBOutlet weak var searchResultsViewer: UITableView!
+    
+}
+
+extension SearchView: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let completion = searchResults[indexPath.row]
+        print("======== Selected Cell ========")
+        print(completion)
+        let searchRequest = MKLocalSearchRequest(completion: completion)
+        let search = MKLocalSearch(request: searchRequest)
+        search.start { (response, error) in
+            //let coordinate = response?.mapItems[0].placemark.coordinate
+            SearchView.currentLoc = (response?.mapItems[0].name)!
+            print(response?.mapItems[0].name)
+        }
+        ViewController.updateLoc(to: SearchView.currentLoc)
+        performSegue(withIdentifier: "placeSelected", sender: self)
+    }
+}
+
+extension SearchView: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let searchResult = searchResults[indexPath.row]
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        print(searchResult)
+        cell.textLabel?.text = searchResult.title
+        cell.detailTextLabel?.text = searchResult.subtitle
+        print("========== CELL ==========")
+        print("\(cell)")
+        return cell
     }
 }
--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -11,15 +11,28 @@ import CoreData
 
 class ViewController: UIViewController, UITextFieldDelegate {
     
+    // TESTING
+    
+    
+    // TESTING
+    
+    private static var currentLoc: String?
     private static let cdManager = CoreDataManager(modelName: "Weather")
     
     class func getCDManager() -> CoreDataManager{
         return cdManager
     }
     
+    class func updateLoc(to newLoc: String) {
+        ViewController.currentLoc = newLoc
+        
+    }
+    
+    @IBOutlet weak var location: UITextField!
     @IBOutlet weak var name: UITextField!
     @IBOutlet weak var units: UITextField!
     var unitTextField = UnitSelector.currentUnit()
+    var locationTextField = SearchView.getCurrentLoc()
 
     
     @IBAction func goToSearchViewController(_ sender: UITextField) {
@@ -28,6 +41,10 @@ class ViewController: UIViewController, UITextFieldDelegate {
     
     func updateUnitTextField() {
         unitTextField = UnitSelector.currentUnit()
+    }
+    
+    func updateLocationField() {
+        locationTextField = SearchView.getCurrentLoc()
     }
     
     @IBAction func selectUnits(_ sender: UITextField) {
@@ -40,6 +57,55 @@ class ViewController: UIViewController, UITextFieldDelegate {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         units.text = unitTextField
+        updateLocationField()
+        location.text = locationTextField
+        
+        
+        // TEST
+        
+        
+        let todoEndpoint: String = "https://jsonplaceholder.typicode.com/todos/1"
+        guard let url = URL(string: todoEndpoint) else {
+            print("Error: cannot create URL")
+            return
+        }
+        print(url)
+        let urlRequest = URLRequest(url: url)
+        let session = URLSession.shared
+//        let task = session.dataTask(with: urlRequest, completionHandler:{ _, _, _ in })
+        let task = session.dataTask(with: urlRequest) { data, response, error in
+            // do stuff with response, data & error here
+            do {
+                guard let todo = try JSONSerialization.jsonObject(with: data!, options: [])
+                    as? [String: Any] else {
+                        print("error trying to convert data to JSON")
+                        return
+                }
+                // now we have the todo
+                // let's just print it to prove we can access it
+                print("The todo is: " + todo.description)
+                
+                // the todo object is a dictionary
+                // so we just access the title using the "title" key
+                // so check for a title and print it if we have one
+                guard let todoTitle = todo["title"] as? String else {
+                    print("Could not get todo title from JSON")
+                    return
+                }
+                print("The title is: " + todoTitle)
+            } catch  {
+                print("error trying to convert data to JSON")
+                return
+            }
+        }
+        task.resume()
+        
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        updateLocationField()
+        super.viewDidAppear(true)
     }
 
     override func didReceiveMemoryWarning() {
